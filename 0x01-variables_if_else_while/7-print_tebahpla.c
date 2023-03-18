cfbd27940ea0bee4251e98dac5872d0d963f2205
Peter Ndindi <peterndindi819@gmail.com>
2023-03-18 02:03:13 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+        char letter = 'z';
+        while (letter >= 'a')
+        {
+                putchar(letter);
+                letter--;
+        }
+        putchar('\n');
+        return (0);
+}
