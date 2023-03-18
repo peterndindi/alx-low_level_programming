556ce0b93b927020b2720813152995b7ba704439
Peter Ndindi <peterndindi819@gmail.com>
2023-03-18 02:37:26 -0700

    comb 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i + 48);
 			putchar(j + 48);
 
-			if (i < 8 || j < 9)
+			if (!(i == 8 && j == 9))
 			{
 				putchar(',');
 				putchar('	');
@@ -27,4 +27,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
